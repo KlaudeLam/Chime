dcfb69b8c240260db61a647602af60feb21ca72e
Kailey Lam <kaileylammyky@gmail.com>
2024-01-26 12:14:55 +0700

    Log out btn

--- dist/firebase-config.js
+++ dist/firebase-config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
-import { getAuth } from 'https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js';
+import { getAuth, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,3 +18,35 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
+const navBtnLogIn = document.getElementById("nav-btn-log-in");
+const navBtnLogOut = document.getElementById("nav-btn-log-out");
+
+const Logout = () => {
+  signOut(auth).then(() => {
+    // Sign-out successful.
+    alert("Sign out successful");
+  }).catch((error) => {
+    // An error happened.
+    alert("Sign out failed")
+  });
+}
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    navBtnLogIn.style.display = "none";
+    navBtnLogOut.style.display = "block";
+    // ...
+  } else {
+    // User is signed out
+    // ...
+    navBtnLogIn.style.display = "block";
+    navBtnLogOut.style.display = "none";
+  }
+});
+
+navBtnLogOut.onclick = () => {
+  Logout();
+}
