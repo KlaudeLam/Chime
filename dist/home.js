0335cf92befdff39d9aa49fae78876252f8825d5
Kailey Lam <kaileylammyky@gmail.com>
2024-01-26 12:12:43 +0700

    Del player code

--- dist/home.js
+++ dist/home.js
@@ -1,24 +1,3 @@
-let btnPlay = document.getElementsByClassName("btnPlay");
-let btnPause = document.getElementsByClassName("btnPause");
-
-for (let j = 0; j < btnPlay.length; j++) {
-    btnPlay[j].onclick = () => {
-        for (let i = 0; i < btnPlay.length; i++) {
-            btnPause[i].style.display = 'block';
-            btnPlay[i].style.display = 'none';
-        }
-    }
-}
-
-for (let j = 0; j < btnPlay.length; j++) {
-    btnPause[j].onclick = () => {
-        for (let i = 0; i < btnPlay.length; i++) {
-            btnPause[i].style.display = 'none';
-            btnPlay[i].style.display = 'block';
-        }
-    }
-}
-
 let hiddenMenu = document.getElementById("hidden-menu");
 
 document.getElementById("btn-hamburger").onclick = () => {
@@ -29,5 +8,7 @@ document.getElementById("btn-hamburger").onclick = () => {
         hiddenMenu.style.display = "none";
     }
 }
+
+
 
 // if logged: btnLogout appear, btnLogin hidden (viceversa)
