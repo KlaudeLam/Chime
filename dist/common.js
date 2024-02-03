053dfca20b6e1162d962a67ec54bb8d02e8adda0
Kailey Lam <kaileylammyky@gmail.com>
2024-02-03 23:44:29 +0700

    Fix HamMenu bug (style.display -> classlist)

--- dist/common.js
+++ dist/common.js
@@ -5,11 +5,11 @@ import { auth } from './firebase-config.js';
 let hiddenMenu = document.getElementById("hidden-menu");
 
 document.getElementById("btn-hamburger").onclick = () => {
-    if (hiddenMenu.style.display == "none") {
+    if (hiddenMenu.classList.contains("hidden")) {
         hiddenMenu.style.position = "fixed";
-        hiddenMenu.style.display = "block";
+        hiddenMenu.classList.remove("hidden");
     } else {
-        hiddenMenu.style.display = "none";
+        hiddenMenu.classList.add("hidden");
     }
 }
 
